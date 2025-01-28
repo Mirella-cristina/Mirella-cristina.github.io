d4931de376650d261fa6a593a8d97d205a17b3c8
Mirella-cristina <mirela.pereira@estudante.ifms.edu.br>
2025-01-28 17:44:17 -0400

    alterações nos outros

--- src/lib/outros.js
+++ src/lib/outros.js
@@ -1,6 +1,38 @@
 export let outros = [
-    { name: "Bootstrap", description: "Uma estrutura CSS gratuita e de código aberto para desenvolvimento web front-end responsivo.", link: "https://getbootstrap.com", image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png" },
-    { name: "Spring Boot", description: "Facilita o uso de estruturas baseadas em Java para criar microsserviços e aplicativos Web.", link: "https://spring.io/projects/spring-boot", image: "https://spring.io/img/projects/spring-boot.svg" },
-    { name: "Flutter", description: "Framework de código aberto para criar aplicativos multiplataforma.", link: "https://flutter.dev", image: "https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png" }
+    { 
+        name: "Bootstrap", 
+        description: "Uma estrutura CSS gratuita e de código aberto para desenvolvimento web front-end responsivo.", 
+        link: "https://getbootstrap.com", 
+        image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png" 
+    },
+    { 
+        name: "Spring Boot", 
+        description: "Facilita o uso de estruturas baseadas em Java para criar microsserviços e aplicativos Web.", 
+        link: "https://spring.io/projects/spring-boot", 
+        image: "https://spring.io/img/projects/spring-boot.svg" 
+    },
+    { 
+        name: "Flutter", 
+        description: "Framework de código aberto para criar aplicativos multiplataforma.", 
+        link: "https://flutter.dev", 
+        image: "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png" ,
+    },
+    {
+        name: "Spring Framework",
+        description: "Framework robusto para desenvolvimento de aplicações empresariais em Java.",
+        link: "https://spring.io/",
+        image: "https://pluralsight2.imgix.net/paths/images/corespring-f9a00f4516.png",
+    },
+    {
+        name: "Hibernate",
+        description: "Framework ORM para mapeamento de objetos Java para bancos de dados relacionais.",
+        link: "https://hibernate.org/",
+        image: "https://blog.frankel.ch/assets/focus/hibernate.svg",
+    },
+    {
+        name: "JavaFX",
+        description: "Framework para construção de aplicações gráficas ricas (GUI) em Java.",
+        link: "https://openjfx.io/",
+        image: "https://cursa.com.br/uploads/thumbnails/course_thumbnails/optimized/course_thumbnail_default_2131667260800.jpg"
+    }
 ];
-
